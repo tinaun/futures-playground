7740e1fb92f25872a1601a959c7090a365ec470f
tinaun <tinagma@gmail.com>
2018-07-31 17:36:37 -0400

    custom future

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
-#![feature(futures_api, async_await, await_macro, never_type, generators, pin)]
+#![feature(arbitrary_self_types, futures_api, async_await, await_macro, never_type, generators, pin)]
 
 use nb::{block, await as nb_await};
 use futures::executor::block_on;
-use futures::join;
+use futures::{join, unsafe_unpinned, Future};
+use futures::task::{Context, Poll};
 
 use std::time::{Instant, Duration};
+use std::mem::PinMut;
 
 /// a dummy led that blocks for 1s before
 /// it changes state
@@ -50,12 +52,13 @@ fn toggle_sync() {
     block!(b.toggle());
 }
 
-fn toggle_async() {
+fn _toggle_async() {
     let a = async {
         let mut a = SlowLed::new();
         nb_await!(a.toggle()) 
         // nb_await is not executor aware - it never calls the waker,
-        // and thus, never is called again after yielding
+        // and thus, never is called again after yielding - running this function calls each 
+        // sub future once and then never again.
     };
 
     let b = async {
@@ -68,7 +71,61 @@ fn toggle_async() {
     });
 }
 
+// you could write a custom executor that calls all of its futures round robin without using a waker,
+// but i decided to write a wrapper type for the built in executor instead. - i imagine that the former
+// approach would make more sense on an embedded system, but i don't have much experience there.
+
+/// a future that wraps a nonblocking function
+struct NbFuture<F, T, E> 
+    where F: FnMut() -> nb::Result<T, E>
+{
+    f: F,
+}
+
+impl<F, T, E> NbFuture<F, T, E> 
+    where F: FnMut() -> nb::Result<T, E>
+{
+    unsafe_unpinned!(f: F); 
+    //convenence macro for getting a mutable reference to a field out of a pinned struct
+}
+
+impl<F, T, E> Future for NbFuture<F, T, E> 
+    where F: FnMut() -> nb::Result<T, E>
+{
+    type Output = Result<T, E>;
+    fn poll(mut self: PinMut<Self>, ctx: &mut Context) -> Poll<Result<T, E>> {
+        match (self.f())() {
+            Ok(t) => Poll::Ready(Ok(t)),
+            Err(nb::Error::WouldBlock) => {
+                ctx.waker().wake();
+                Poll::Pending
+            },
+            Err(nb::Error::Other(e)) => Poll::Ready(Err(e))
+        }
+    }
+}
+
+fn would_block<F, T, E>(f: F) -> NbFuture<F, T, E> 
+    where F: FnMut() -> nb::Result<T, E>
+{
+    NbFuture {
+        f
+    }
+}
+
+fn toggle_async_two() {
+    let mut a = SlowLed::new();
+    let a = would_block(move || a.toggle());
+
+    let mut b = SlowLed::new();
+    let b = would_block(move || b.toggle());
+
+    block_on(async {
+        join!(a, b);
+    });
+}
+
 fn main() {
     println!("{:?}", time_it(toggle_sync));
-    println!("{:?}", time_it(toggle_async));
+    println!("{:?}", time_it(toggle_async_two));
 }
