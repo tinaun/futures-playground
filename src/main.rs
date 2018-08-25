3e34415ba47f09368feba593e491ee6b37f1b53c
tinaun <tinagma@gmail.com>
2018-08-25 03:54:02 -0400

    updated example

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
-#![feature(async_await, await_macro)]
+#![feature(pin, async_await, await_macro)]
 
 use hyper::Client;
-use hyper::rt::Stream;
 use futures::compat::*;
+use futures::stream::{StreamExt};
 use futures::future::{TryFutureExt, FutureExt};
+
+use pin_utils::pin_mut;
 
 use tokio::runtime::Runtime;
 use std::io::{self, Write};
@@ -16,14 +18,16 @@ fn main() {
         let res = await!(fut).unwrap();
         println!("{}", res.status());
 
-        let body = res.into_body().concat2().compat();
-        let body = await!(body).unwrap();
-        
-        io::stdout()
-            .write_all(&body)
+        let body = res.into_body().compat();
+
+        pin_mut!(body);
+        while let Some(Ok(chunk)) = await!(body.next()) {
+            let _ = io::stdout()
+            .write_all(&chunk)
             .map_err(|e| {
                 panic!("example expects stdout is open, error={}", e)
-            })
+            });
+        }
     };
 
     let mut rt = Runtime::new().unwrap();
